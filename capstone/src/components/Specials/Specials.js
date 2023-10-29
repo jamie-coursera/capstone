07aa56674840c5fa5120b9862da0094012298e8f
deluxejamie <86162936+deluxejamie@users.noreply.github.com>
2023-10-29 17:44:18 +0000

    specials done

---
+++ capstone/src/components/Specials/Specials.js
@@ -0,0 +1,63 @@
+import Button from '../Button/Button'
+import GreekSalad from '../../assets/greek salad.jpg'
+import './Specials.css'
+function Special() {
+    return (
+        <>
+
+            <section id="titleBox">
+                <article id='specialstitle'>
+                    Specials
+                </article>
+                <article id='specialsbutton'>
+                    <Button text='Order Online' link='/order'/>
+                </article>
+             </section>
+
+
+
+
+
+
+             <section id="specialsBox">
+                <article>
+                    <img src={GreekSalad} alt='a greek salad' className="specialsImage"/>
+                    <header>
+                        <h1>Greek Salad </h1>
+                        <span className='specialsprice'>$12.99</span>
+                    </header>
+                    <body>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.                        
+                    </body>
+                    
+                </article>
+
+                <article>
+                    <img src={GreekSalad} alt='a greek salad' className="specialsImage"/>
+                    <header>
+                        <h1>Greek Salad </h1>
+                        <span className='specialsprice'>$12.99</span>
+                    </header>
+                    <body>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </body>
+                    
+                </article>
+
+                <article>
+                    <img src={GreekSalad} alt='a greek salad' className="specialsImage"/>
+                    <header>
+                    <h1>Greek Salad </h1>
+                    <span className='specialsprice'>$12.99</span>
+                    </header>
+                    <body>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </body>
+                    
+                </article>
+             </section>
+         </>
+    )
+}
+
+export default Special
--- capstone/src/pages/Home.js
+++ capstone/src/pages/Home.js
@@ -1,8 +1,10 @@
 import Hero from '../components/Hero/Hero'
+import Specials from '../components/Specials/Specials'
 function Home() {
     return (
         <>
         <Hero/>
+        <Specials/>
         </>
     )
 }
