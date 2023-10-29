a65cc4cc84cb4189f0042d425f5569fdd1a692cd
deluxejamie <86162936+deluxejamie@users.noreply.github.com>
2023-10-29 15:47:33 +0000

    hero done

--- capstone/src/components/Hero/Hero.js
+++ capstone/src/components/Hero/Hero.js
@@ -4,17 +4,18 @@ import Button from '../Button/Button'
 function Hero() {
     return (
         <section>
-            <article style={{flex:1}}>
+            <article style={{flex:1,paddingLeft:"23vw"}}>
 
                     <h1 id="title">Little Lemon</h1>
                     <h2 id="descriptor">Chicago</h2>
 
                     <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+                    <br></br>
                     <Button link="/reserve" text="Reserve a Table" id="reserveBtn"/>
 
             </article>
 
-            <article style={{flex:1}}>
+            <article style={{flex:1,paddingRight:"23vw"}}>
                 <img src={ChefImg} alt="Food at Little Lemon"/>
             </article>
         </section>
