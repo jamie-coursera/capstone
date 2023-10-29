f09f870e3961681d937320398c30c9acef91a1e5
deluxejamie <86162936+deluxejamie@users.noreply.github.com>
2023-10-29 15:05:31 +0000

    button component

---
+++ capstone/src/components/Button/Button.js
@@ -0,0 +1,18 @@
+import {Link} from 'react-router-dom'
+import './Button.css'
+function Button({text,link}) {
+    return (
+
+        <span className="outline">
+            <span className='button-yellow'>
+            <Link to={link}>
+                {text}
+            </Link>
+            </span>
+        </span>
+        
+
+    )
+}
+
+export default Button
