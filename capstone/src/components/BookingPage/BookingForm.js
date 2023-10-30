bdd46511ec9b79c6f1b9fc829ed4122b2fd36243
deluxejamie <86162936+deluxejamie@users.noreply.github.com>
2023-10-30 15:35:30 +0000

    booking adjustments

--- capstone/src/components/BookingPage/BookingForm.js
+++ capstone/src/components/BookingPage/BookingForm.js
@@ -2,10 +2,7 @@ import {useState} from 'react';
 function handleSubmit(e) {
     e.preventDefault();
 }
-function BookingForm() {
-
-    const [availableTimes,setAvailableTimes] = useState(['17:00','18:00','19:00','20:00','21:00','22:00']) 
-    const [date,setDate] = useState({value:null});
+function BookingForm({availableTimes,dateta:{date,setDate}}) {
     const [time,setTime] = useState({value:null});
     const [guests,setGuests] = useState({value:null});
     const [occasion,setOccasion] = useState({value:'Birthday'});
@@ -14,12 +11,15 @@ function BookingForm() {
     <form onSubmit={handleSubmit}>
         <label htmlFor ="res-date" >Choose date</label>
     <input type="date" id="res-date" value={date.value} onChange={(e) => {setDate({...date,value:e.target.value})}}/>
+    <br/>
     <label htmlFor ="res-time">Choose time</label>
     <select id="res-time" value={time.value} onChange={(e) => {setTime({...time,value:e.target.value})}}>
         {availableTimes.map(a => <option>{a}</option>)}
     </select>
+    <br/>
     <label htmlFor="guests">Number of guests</label>
     <input type="number" placeholder="1" min="1" max="10" id="guests" value={guests.value} onChange={(e) => {setGuests({...guests,value:e.target.value})}}/>
+    <br/>
     <label htmlFor ="occasion">Occasion</label>
     <select id="occasion" value={occasion.value} onChange={(e) => {setOccasion({...occasion,value:e.target.value})}}>
         <option>Birthday</option>
--- capstone/src/components/BookingPage/BookingPage.js
+++ capstone/src/components/BookingPage/BookingPage.js
@@ -1,9 +1,24 @@
 import BookingForm from './BookingForm.js';
-
+import {useReducer, useState} from 'react'
 function BookingPage() {
+
+    function updateTimes(state) {
+        return dispatch(state)
+    }
+
+    function initlializeTimes() {
+
+        return ['17:00','18:00','19:00','20:00','21:00','22:00']
+
+    }
+    
+    
+    const [date,setDate] = useState({value:null});
+    const [availableTimes,dispatch] = useReducer(updateTimes,date,initlializeTimes);
     return (
         <>
-        <BookingForm/>
+        <title>Reserve a Table</title>
+        <BookingForm availableTimes={availableTimes} dateta={{date,setDate} /*this is an object abbreviation which means {date:date,setDate:setDate}*/}/> 
         </>
     )
 }
